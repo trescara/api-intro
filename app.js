3e16ddbd46a18906120291333d30c2711b96d1ef
Matt Winzer <matt.winzer@gmail.com>
2018-03-20 12:26:19 -0600

    added more steps for part two

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const baseUrl = 'https://swapi.co/api/'
 const characterSection = document.querySelector('.characters')
 
-// Write a function that GETs information about Luke Skywalker and add his name to the page
+// Write a function that GETs information about Luke Skywalker
+  // and add his name to the page
 getLuke()
 
 
@@ -29,3 +30,31 @@ function getLuke() {
 // Modify the getLuke function to also add:
   // Luke's hair color
   // Luke's height
+// Append these attributes as text in a <li>
+// to the ul with the class of 'attributes'
+
+
+// Uh oh, Luke's attributes are ABOVE his name!?
+  // Explain why this happened
+  // Modify the getLuke function to ensure Luke's attributes are below his name
+
+
+// Write a function called getCharacters that:
+  // Makes a fetch request to the baseUrl + '/people/ endpoint
+  // console.log the resulting response
+
+
+// Modify the getCharacters function to loop through the results
+  // console.log each character in the array
+    // (see what you're working with)
+  // Capture each character's attributes in variables:
+    // name
+    // hair color
+    // height
+  // console.log each variable
+
+
+// Modify the getCharacters function to append a section for each character
+  // Container element (<div> or <section>)
+  // <h3> with the character's name
+  // <ul> with  <li>s for hair color and height
