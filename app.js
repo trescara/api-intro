7e69bc43d079eaf70bcba0f8d8c19baa15a2e33b
Matt Winzer <matt.winzer@gmail.com>
2018-03-21 10:42:52 -0600

    breakout 2 completed

--- app.js
+++ app.js
@@ -13,16 +13,37 @@ function getLuke() {
       return response.json()
     })
     .then(function(data) {
-      var name = data.name
+      // var name = data.name
+      // console.log(data)
 
-      // Create an h2 that has the text of 
-      // Luke's name
+      // // Create an h2 that has the text of 
+      // // Luke's name
+      // var skywalker = document.createElement('h2')
+      // skywalker.innerText = name
+
+      // // Append that h2 to the section
+      // // w/the class .characters
+      // characterSection.appendChild(skywalker)
+      var name = data.name
+      var colorHair = data.hair_color
+      var howTall = data.height
       var skywalker = document.createElement('h2')
       skywalker.innerText = name
 
-      // Append that h2 to the section
-      // w/the class .characters
-      characterSection.appendChild(skywalker)
+      var hairLi = document.createElement('li')
+      var heightLi = document.createElement('li')
+      hairLi.textContent = colorHair
+      heightLi.textContent = howTall
+
+      var attributes = document.querySelector('.attributes')
+
+      // skywalker.innerText = colorHair
+      // skywalker.innerText = howTall
+      // console.log(skywalker)
+      characterSection.insertBefore(skywalker, attributes)
+      attributes.appendChild(hairLi)
+      attributes.appendChild(heightLi)
+
     })
 
 }
@@ -40,9 +61,34 @@ function getLuke() {
 
 
 // Write a function called getCharacters that:
-  // Makes a fetch request to the baseUrl + '/people/ endpoint
+  // Makes a fetch request to the baseUrl + 'people/ endpoint
   // console.log the resulting response
 
+// getCharacters()
+
+function getCharacters() {
+
+  fetch(baseUrl + 'people')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (people) {
+      // console.log(people)
+      // Write a loop
+      var peopleArray = people.results
+      for (var i = 0; i < peopleArray.length; i++) {
+
+        console.log(people.results[i]);
+
+        var person = people.results[i]
+        var name = person.name
+        var h3 = document.createElement('h3')
+        h3.textContent = name
+        characterSection.appendChild(h3)
+
+      }
+    })
+}
 
 // Modify the getCharacters function to loop through the results
   // console.log each character in the array
@@ -58,3 +104,4 @@ function getLuke() {
   // Container element (<div> or <section>)
   // <h3> with the character's name
   // <ul> with  <li>s for hair color and height
+  // Check out the innerHTML method
